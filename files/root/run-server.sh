abd3e57833d84e3e12f434e4332f605677ea7e2f
bouroo <bouroo@gmail.com>
2016-07-01 19:30:54 +0700

    Update dockerfile

--- files/root/run-server.sh
+++ files/root/run-server.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-chown -R www-data:www-data /srv/*
-
 if [ ! -d /var/lib/mysql/mysql ]; then
 	mysql_install_db
+
+	# mysql_secure_installation
+	USER=$(grep -m 1 user /etc/mysql/debian.cnf | cut -d= -f2 | tr -d "[[:space:]]")
+	PASSWD=$(grep -m 1 password /etc/mysql/debian.cnf | cut -d= -f2 | tr -d "[[:space:]]")
+	mysql -e "DELETE FROM mysql.user WHERE User='';"
+	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	mysql -e "DROP DATABASE IF EXISTS test;"
+	mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+	mysql -e "FLUSH PRIVILEGES"
 fi
 
 chown mysql:mysql -R /var/lib/mysql
