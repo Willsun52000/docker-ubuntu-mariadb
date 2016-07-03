a11b95ebfa0eaffd80478b8da86e52824a463d66
bouroo <bouroo@gmail.com>
2016-07-03 11:59:37 +0700

    Allow remote client

--- files/root/run-server.sh
+++ files/root/run-server.sh
@@ -7,20 +7,19 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 	USER=$(grep -m 1 user /etc/mysql/debian.cnf | cut -d= -f2 | tr -d "[[:space:]]")
 	PASSWD=$(grep -m 1 password /etc/mysql/debian.cnf | cut -d= -f2 | tr -d "[[:space:]]")
 	mysql -e "DELETE FROM mysql.user WHERE User='';"
-	mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	#mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 	mysql -e "DROP DATABASE IF EXISTS test;"
 	mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 	mysql -e "FLUSH PRIVILEGES"
 fi
 
 chown mysql:mysql -R /var/lib/mysql
+chown mysql:mysql -R /var/log/mysql
 
 #service supervisor start
 service ssh start
 service rsyslog start
 service mysql start
 service cron start
-
-openssl dhparam -dsaparam -out /etc/ssl/dhparam.pem 4096
 
 tail -f /dev/null
